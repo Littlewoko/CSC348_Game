13330528ba51dc437bc57f6d581b974a0ae0ce29
Bradley Watkins <56637748+Littlewoko@users.noreply.github.com>
2022-02-11 18:59:49 +0000

    variable speed

--- Assets/Scripts/Flight/ShipController.cs
+++ Assets/Scripts/Flight/ShipController.cs
@@ -14,6 +14,7 @@ public class ShipController : MonoBehaviour
     private Rigidbody2D rb2D;
     private float currentRotationSpeed = 0f;
     private float signedAngle;
+    private float speedPercentage;
     private Vector2 toTarget = new Vector2();
     private Transform myTransform;
 
@@ -23,22 +24,45 @@ public class ShipController : MonoBehaviour
     }
 
     private void FixedUpdate() {
-        UpdateVelocity();
-        updateRoation();
+        if(myTarget.position.x != myTransform.position.x && myTarget.position.y != myTransform.position.y) {
+            StopMoving();
+            UpdateVelocity();
+            updateRotation();
+        } else {
+            UpdateVelocity();
+            updateRotation();
+        }
     }
 
     private void UpdateVelocity() {
-        rb2D.velocity = mySpeed * myForward.up;
+        signedAngle = GetAngleToTarget();
+        speedPercentage = signedAngle / 180;
+        if (speedPercentage < 0) {
+            speedPercentage = -speedPercentage; 
+        }
+        if (speedPercentage < 0.4) {
+            speedPercentage = (float)0.4;
+        }
+        rb2D.velocity = (mySpeed * speedPercentage) * myForward.up;
     }
 
-    private void updateRoation() {
+    private void updateRotation() {
+        signedAngle = GetAngleToTarget();
+
+        currentRotationSpeed = signedAngle / 90f;
+
+        rb2D.angularVelocity = currentRotationSpeed * maxRotationSpeed;
+    }
+
+    private void StopMoving() {
+        rb2D.velocity = 0 * myForward.up;
+        currentRotationSpeed = 0;
+    }
+
+    private float GetAngleToTarget() {
         toTarget.x = myTarget.position.x - myTransform.position.x;
         toTarget.y = myTarget.position.y - myTransform.position.y;
 
-        signedAngle = Vector2.SignedAngle(myForward.up, toTarget);
-
-        currentRotationSpeed = signedAngle / 180f;
-
-        rb2D.angularVelocity = currentRotationSpeed * maxRotationSpeed;
+        return Vector2.SignedAngle(myForward.up, toTarget);
     }
 }
