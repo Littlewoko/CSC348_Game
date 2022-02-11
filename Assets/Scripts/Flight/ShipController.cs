61ef162c227c48cfa1e11a1a822286a2fde94c16
Bradley Watkins <56637748+Littlewoko@users.noreply.github.com>
2022-02-11 17:37:42 +0000

    target path finding, ship chases and rotataes around target

--- Assets/Scripts/Flight/ShipController.cs
+++ Assets/Scripts/Flight/ShipController.cs
@@ -8,12 +8,18 @@ public class ShipController : MonoBehaviour
 {
     [SerializeField] private Transform myForward;
     [SerializeField] private float mySpeed;
-    [SerializeField] private float myRotationSpeed;
+    [SerializeField] private float maxRotationSpeed;
+    [SerializeField] private Transform myTarget;
 
     private Rigidbody2D rb2D;
+    private float currentRotationSpeed = 0f;
+    private float signedAngle;
+    private Vector2 toTarget = new Vector2();
+    private Transform myTransform;
 
     private void Awake() {
         rb2D = GetComponent<Rigidbody2D>();
+        myTransform = transform;
     }
 
     private void FixedUpdate() {
@@ -26,6 +32,13 @@ public class ShipController : MonoBehaviour
     }
 
     private void updateRoation() {
-        rb2D.angularVelocity = myRotationSpeed;
+        toTarget.x = myTarget.position.x - myTransform.position.x;
+        toTarget.y = myTarget.position.y - myTransform.position.y;
+
+        signedAngle = Vector2.SignedAngle(myForward.up, toTarget);
+
+        currentRotationSpeed = signedAngle / 180f;
+
+        rb2D.angularVelocity = currentRotationSpeed * maxRotationSpeed;
     }
 }
